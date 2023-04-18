df1d3ab1a1645209fe38bd48f3f1efb8279ffb21
Elena Moser <elemoser@gmail.com>
2023-04-18 10:58:35 +0200

    Refactor Card class acc. linter errors

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -4,14 +4,29 @@ namespace App\Card;
 
 class Card
 {
+    /**
+     * @var array<string> $value
+     */
     protected $value;
+
+    /**
+     * @var array<string> $suites
+     */
     public $suites;
+
+    /**
+     * @var int $minValue
+     */
     public $minValue;
+
+    /**
+     * @var int $maxValue
+     */
     public $maxValue;
 
     public function __construct(
-        $suite = null,
-        $number = null
+        string $suite = null,
+        int $number = null
     ) {
         $this->value = [];
         $this->suites = ["hjärter", "klöver", "ruter", "spader"];
@@ -27,46 +42,47 @@ class Card
             $number = random_int($this->minValue, $this->maxValue);
         }
 
-        if ($number == 1) {
-            $number = "ess";
-        }
-
-        if ($number == 11) {
-            $number = "knekt";
-        }
-
-        if ($number == 12) {
-            $number = "dam";
-        }
-
-        if ($number == 13) {
-            $number = "kung";
-        }
+        $number = $this->convertCardNumberToString($number);
 
         $this->value = [$suite, $number];
     }
 
-    public function draw(): array
+    /**
+     * @return string
+     */
+    protected function convertCardNumberToString(int $integer)
+    {
+        $number = (string) $integer;
+
+        if ($number == "1") {
+            $number = "ess";
+        }
+
+        if ($number == "11") {
+            $number = "knekt";
+        }
+
+        if ($number == "12") {
+            $number = "dam";
+        }
+
+        if ($number == "13") {
+            $number = "kung";
+        }
+
+        return $number;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function draw()
     {
         // Generate a new random suite and number
         $randSuites = array_rand($this->suites, 1);
         $number = random_int($this->minValue, $this->maxValue);
 
-        if ($number == 1) {
-            $number = "ess";
-        }
-
-        if ($number == 11) {
-            $number = "knekt";
-        }
-
-        if ($number == 12) {
-            $number = "dam";
-        }
-
-        if ($number == 13) {
-            $number = "kung";
-        }
+        $number = $this->convertCardNumberToString($number);
 
         // $this->value = ["klöver", "ess"];
         $this->value = [$this->suites[$randSuites], $number];
@@ -74,17 +90,26 @@ class Card
         return $this->value;
     }
 
-    public function getValue(): array
+    /**
+     * @return array<string>
+     */
+    public function getValue()
     {
         return $this->value;
     }
 
-    public function getAsString(): string
+    /**
+     * @return string
+     */
+    public function getAsString()
     {
         return join(" ", $this->value);
     }
 
-    public function getColor(): string
+    /**
+     * @return string
+     */
+    public function getColor()
     {
         $color = "black";
 
