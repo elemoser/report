a5f1d64d17393d59ebfeb0aa3c885212f9aaa13e
Elena Moser <elemoser@gmail.com>
2023-04-26 11:18:56 +0200

    Add api/game route

--- src/Controller/JsonApiController.php
+++ src/Controller/JsonApiController.php
@@ -110,7 +110,6 @@ class JsonApiController extends AbstractController
 
         $deck = $session->get("deck");
 
-        // Draw random cards
         if (!$num) {
             $num = 2;
         }
@@ -156,15 +155,58 @@ class JsonApiController extends AbstractController
         }
 
         if ($drawnCards instanceof CardCollection) {
-            // var_dump($drawnCards);
-
-            // if (count($drawnCards) === 1) {
-            //     $data["CardDrawn"] = $drawnCards[0]->getAsString();
-            //     $data["CardsColor"] = $drawnCards[0]->getColor();
-            // } else {
             $data["CardsDrawn"] = $drawnCards->getStrings();
             $data["CardsColor"] = $drawnCards->getColors();
-            // }
+        }
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/game", name: "api_game21", methods: ['POST'])]
+    public function apiGame21(
+        SessionInterface $session
+    ): Response
+    {
+        $data = [
+            "currentPlayer" => "",
+            "playerCards" => [],
+            "playerCardTotal" => 0,
+            "bankCards" => [],
+            "bankCardTotal" => 0,
+            "winner" => "",
+            "loser" => ""
+        ];
+
+        if ($session->has("game")) {
+            $game = $session->get("game");
+            $currentPlayer = $game->getCurrentPlayerInQueue();
+            $data["currentPlayer"] = $currentPlayer->name;
+            $winStatus = $game->checkWinStatus();
+
+            if ($winStatus["loser"]) {
+                $data["winner"] = $winStatus["winner"];
+                $data["loser"] = $winStatus["loser"];
+            }
+        }
+
+        if ($session->has("player_cards")) {
+            $data["playerCards"] = $session->get("player_cards");
+        }
+
+        if ($session->has("player_card_total")) {
+            $data["playerCardTotal"] = $session->get("player_card_total");
+        }
+
+        if ($session->has("bank_cards")) {
+            $data["bankCards"] = $session->get("bank_cards");
+        }
+
+        if ($session->has("bank_card_total")) {
+            $data["bankCardTotal"] = $session->get("bank_card_total");
         }
 
         $response = new JsonResponse($data);
