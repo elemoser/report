1d93c96ce33172a80cfa9f904a65af9b27187ac1
Elena Moser <elemoser@gmail.com>
2023-05-29 17:20:01 +0200

    Add game mechanics

--- src/AdventureGame/Game.php
+++ src/AdventureGame/Game.php
@@ -2,6 +2,7 @@
 
 namespace App\AdventureGame;
 
+use App\Entity\AdventureRoom;
 // use Doctrine\ORM\EntityManagerInterface;
 // use Doctrine\ORM\EntityRepository;
 
@@ -45,53 +46,167 @@ class Game
      * This method ...
      * @return void
      */
-    public function initiate(){
+    public function initiate()
+    {
         // get information about the new place and return?
         // update $currentRoom and $visited
     }
 
     /**
-     * This method ...
-     * @return void
+     * This method returns the name of the current location.
+     * @return string
      */
-    public function go(string $place){
-        // get information about the new place and return?
-        // update $currentRoom and $visited
+    public function getCurrentRoom()
+    {
+        return $this->currentRoom;
     }
 
-        /**
-     * This method ...
+    /**
+     * This method updates the internal game values.
+     * @return void
+     */
+    public function goTo(string $place)
+    {
+        if ($place != $this->currentRoom) {
+            $this->currentRoom = $place;
+        }
+
+        if (in_array($place, $this->visited)) {
+            array_push($this->visited, $place);
+        }
+    }
+
+    /**
+     * This method returns an array containing all the locations visited.
+     * @return array<int, string>
+     */
+    public function getVisitedRooms()
+    {
+        return $this->visited;
+    }
+
+    /**
+     * This method returns an array containing all the possible direction from location.
+     * @return string
+     */
+    public function getDirections(AdventureRoom $roomObject)
+    {
+        $message = "From here you can go ";
+        $directions = [];
+
+        if ($roomObject->getNorth() != null) {
+            $directions[] = "north to the ".$roomObject->getNorth();
+        }
+
+        if ($roomObject->getEast() != null) {
+            $directions[] = "east to the ".$roomObject->getEast();
+        }
+
+        if ($roomObject->getSouth() != null) {
+            $directions[] = "south to the ".$roomObject->getSouth();
+        }
+
+        if ($roomObject->getWest() != null) {
+            $directions[] = "west to the ".$roomObject->getWest();
+        }
+
+        $message += $directions[0];
+
+        // add other items to $message separated by a coma
+        // if (count($directions) > 1) {
+        //     join(", ", $directions);
+        // }
+
+
+        return $this->visited;
+    }
+
+    /**
+     * This method checks if the basket contains the ingredients for the cake.
      * @return boolean
      */
-    private function checkIngredients(){
-        // check if in kitchen?
-        // check if $this->basket includes all required ingredients
+    public function checkIngredients()
+    {
+        $complete = true;
+        $ingredients = [
+            "salt",
+            "sugar",
+            "jam",
+            "vanilla",
+            "flour",
+            "strawberries",
+            "cream",
+            "milk",
+            "eggs",
+            "butter"
+        ];
+
+        foreach ($this->basket as $item) {
+            if (!in_array($item, $ingredients)) {
+                $complete = false;
+            }
+        }
+
+        return $complete;
     }
 
     /**
      * This method ...
-     * @return void
+     * @return string
      */
-    public function inspect(string $object){
+    public function inspect(string $object)
+    {
         // returns the description for a place or an item
+        return $object;
     }
 
     /**
-     * This method ...
-     * @return void
+     * This method adds the given item to the basket.
+     * @return string
      */
-    public function pickUp(string $item){
-        // update $basket
-        // return string like "You have picked up ${item}"
-        // if $this->basket is full return "Your basket is full"
+    public function pickUp(string $item)
+    {
+        $message = "The basket is full.";
+
+        if (count($this->basket) < 10) {
+            $message = "You have picked up the ".$item.".";
+
+            if (!in_array($item, $this->basket)) {
+                array_push($this->basket, $item);
+                $message = "You already have picked up ".$item.".";
+            }
+        }
+
+        return $message;
     }
 
     /**
-     * This method ...
-     * @return void
+     * This method removes the given item from the basket.
+     * @return string
      */
-    public function putDown(string $item){
-        // update $basket
-        // return string "You put back ${item}"
+    public function putDown(string $item)
+    {
+        $message = "The basket is empty.";
+
+        if (count($this->basket) > 0) {
+            $message = "The ".$item." are not in the basket.";
+
+            if (in_array($item, $this->basket)) {
+                $key = array_search($item, $this->basket);
+                unset($this->basket[$key]);
+                $message = "You put down the ".$item.".";
+            }
+        }
+
+        return $message;
+    }
+
+    /**
+     * This method return the basket as an array.
+     * @return array<int, string>
+     */
+    public function getBasket()
+    {
+        return $this->basket;
     }
 }
--- src/Controller/AdventureGameController.php
+++ src/Controller/AdventureGameController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
+use App\AdventureGame\Game;
+use App\Entity\AdventureItems;
+use App\Entity\AdventureRoom;
+use App\Repository\AdventureItemsRepository;
+use App\Repository\AdventureRoomRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +23,89 @@ class AdventureGameController extends AbstractController
         return $this->render('adventure/index.html.twig');
     }
 
-    #[Route('/proj/play', name: 'proj_game')]
-    public function projectPlay(): Response
+    #[Route('/proj/init/game', name: 'proj_init_game')]
+    public function projectInitGame(
+        SessionInterface $session
+    ): Response
     {
-        return $this->render('adventure/game.html.twig');
+        $game = new Game();
+        $session->set("adventure", $game);
+
+        return $this->redirectToRoute('proj_game');
+    }
+
+    #[Route('/proj/play', name: 'proj_game')]
+    public function projectPlay(
+        AdventureRoomRepository $roomRepository,
+        AdventureItemsRepository $itemsRepository,
+        SessionInterface $session
+    ): Response {
+        $data = [
+            "place" => "",
+            "image" => "",
+            "description" => "",
+            "directions" => "",
+            "basket" => []
+        ];
+
+        if (!$session->get("adventure")) {
+            return $this->redirectToRoute('proj_init_game');
+        }
+
+        $game = $session->get("adventure");
+        $currentRoom = $game->getCurrentRoom();
+
+        $room = $roomRepository->findOneBy(['name' => $currentRoom]);
+
+        if ($room) {
+            $data["place"] = $room->getName();
+            $data["image"] = $room->getImage();
+            $data["description"] = 'You are at '.$room->getDescription().'.';
+            $directions = [];
+        }
+
+
+        $items = $itemsRepository->findAll();
+
+        if ($items) {
+            $itemsInCurrentRoom = [];
+
+            foreach ($items as $item) {
+                $itemLocation = $item->getRoom();
+                if ($itemLocation == $currentRoom) {
+                    array_push($itemsInCurrentRoom, $item->getImage());
+                }
+            }
+
+            $data["basket"] = $itemsInCurrentRoom;
+        }
+
+
+        return $this->render('adventure/game.html.twig', $data);
+    }
+
+    #[Route('/proj/game/input', name: 'proj_game_input', methods: 'POST')]
+    public function projectGameInput(
+        AdventureRoomRepository $roomRepository,
+        AdventureItemsRepository $itemsRepository,
+        SessionInterface $session,
+        Request $request
+    ): Response
+    {
+        if (!$session->get("adventure")) {
+            return $this->redirectToRoute('proj_init_game');
+        }
+
+        $inputStr = (string) $request->request->get('input');
+        $input = explode(" ", $inputStr);
+        
+        if (in_array("go", $input)) {
+            $game = $session->get("adventure");
+            $game->goTo($input[1]);
+            $session->set("adventure", $game);
+        }
+
+        return $this->redirectToRoute('proj_game');
     }
 
     #[Route('/proj/about', name: 'proj_about')]
