6fe9771abd223d7f9bf7a2ac5d42d34444edf55d
Elena Moser <elemoser@gmail.com>
2023-05-29 17:14:10 +0200

    Create bash script to download csv with data

---
+++ public/data/download-csv.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+url="https://docs.google.com/spreadsheets/d/1iNe0jUhg4FoOCejpKxICD-c73C0CrvS-MNe5sR2tgzM/gviz/tq?tqx=out:csv&sheet"
+
+for target in Items Room; do
+    printf "%s\\n" "$target"
+    curl --silent "$url=$target" > "$target.csv"
+done
+
+ls -l -- *.csv
+file -- *.csv
